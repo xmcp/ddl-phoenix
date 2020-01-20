546b017833990cffb8493c96aa372d655ada5164
xmcp <xiaoyuanan2000@sina.com>
2020-01-21 00:08:24 +0800

    add sharing, etc

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 import {Modals} from './Modals';
 import {AppHeader} from './AppHeader';
@@ -11,16 +12,21 @@ import './App.less';
 import './task_colors.less';
 
 export function App(props) {
+    // update components every hour, so date handling will be correct
+    let timetag=useSelector((state)=>(
+        (state.loading && state.loading.last_update_time) ? Math.floor(+state.loading.last_update_time/3600000) : 0
+    ));
+
     return (
         <div>
-            <Modals />
+            <Modals key={timetag} />
             <AppHeader />
             <div className="skip-header width-container">
                 <StickyMsgsView />
             </div>
             <div className="width-container-leftonly app-main">
-                <TodoView />
-                <MainListView />
+                <TodoView key={timetag} />
+                <MainListView key={timetag} />
             </div>
             <Footer />
         </div>
