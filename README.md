60bfdd2284e3f5e7f8a8f449fc065f5e7bc3b1d1
xmcp <xiaoyuanan2000@sina.com>
2020-01-21 13:54:30 +0800

    add build instruction

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Phoenix <small>from _Project Fire Bird_</small>
+
+Online demo for PKU Helper users at https://pkuhelper.pku.edu.cn/ddl/
+
+## Build Instruction for Self-Hosting
+
+1. Set up your backend server (see instruction from [Bee](https://github.com/pkuhelper-web/bee))
+2. `npm install`
+3. Edit `src/welcomePage.example.js` to implement your token-based account system and registration system
+4. Rename `src/welcomePage.example.js` to `src/welcomePage.js`
+5. Edit `src/state/sister.js` and change `SISTER_ROOT` to the base URL of your backend server
+6. `npm run build`
+7. Host the `build/` folder
--- src/welcome/WelcomePage.example.js
+++ src/welcome/WelcomePage.example.js
@@ -4,11 +4,10 @@ import {Result, Input, Button} from 'antd';
 
 import {do_register} from '../state/actions';
 import {WithFooter} from '../app/Footer';
-import {LoginPopup} from '../infrastructure/widgets';
 
 import fire_bird_logo from '../fire_bird_bw.png';
 
-export function WelcomePage(props) {
+export function WelcomePageExample(props) {
     const dispatch=useDispatch();
     const [regcode,set_regcode]=useState('');
 
@@ -16,14 +15,15 @@ export function WelcomePage(props) {
         <WithFooter>
             <Result
                 icon={<img src={fire_bird_logo} style={{height: '200px'}} alt="fire bird logo" />}
-                title="欢迎加入「不咕计划」"
+                title="不咕计划"
                 subTitle="开源的在线 Deadline 管理工具"
             />
             <div className="center">
+                <p>在这里接入邀请注册系统</p>
                 <p>
                     <Input.Search
                         value={regcode} onChange={(e)=>set_regcode(e.target.value)} style={{width: '280px'}} autoFocus={true}
-                        placeholder="内测期间需要填写邀请码" enterButton="注册" onSearch={()=>dispatch(do_register(regcode))}
+                        placeholder="邀请码" enterButton="注册" onSearch={()=>dispatch(do_register(regcode))}
                     />
                 </p>
                 <br />
@@ -34,15 +34,17 @@ export function WelcomePage(props) {
 
 export function AskTokenPage(props) {
     return (
-        <LoginPopup token_callback={props.on_got_token}>{(do_popup)=>(
-            <WithFooter>
-                <Result
-                    status="info"
-                    title="不咕计划"
-                    subTitle="开源的在线 Deadline 管理工具"
-                    extra={<Button type="primary" onClick={do_popup}>用 PKU Helper 账号登录</Button>}
-                />
-            </WithFooter>
-        )}</LoginPopup>
+        <WithFooter>
+            <Result
+                status="info"
+                title="不咕计划"
+                subTitle="开源的在线 Deadline 管理工具"
+                extra={<Button type="primary" onClick={()=>{
+                    let token=prompt('input token');
+                    if(token)
+                        props.on_got_token(token);
+                }}>在这里接入用户系统</Button>}
+            />
+        </WithFooter>
     )
 }
