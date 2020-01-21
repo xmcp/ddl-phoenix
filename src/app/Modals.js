edfd7c253fab6e9980a7b3e8b4e2ef430f82c58b
xmcp <xiaoyuanan2000@sina.com>
2020-01-21 13:41:35 +0800

    improve reordering

--- src/app/Modals.js
+++ src/app/Modals.js
@@ -29,7 +29,7 @@ function SharingHelp(props) {
                 <li>在 “编辑类别” 对话框中勾选 “分享给其他用户”。</li>
                 <li>保存后，在类别的下拉菜单中点击 “复制分享ID” 并发送给别人。</li>
                 <li><b style={{color: 'red'}}>如果你取消勾选 “分享给其他用户”，分享ID将失效，但已经导入的用户仍能继续使用。</b></li>
-                <li><b style={{color: 'red'}}>如果你取消分享后删除这个类别，导入了此ID的用户将失去这个类别的所有数据。</b></li>
+                <li><b style={{color: 'red'}}>如果你取消分享后删除这个类别，已经导入的用户仍能继续使用，但没有人能编辑。</b></li>
             </ul>
             <p>导入他人的分享：</p>
             <ul>
@@ -362,21 +362,23 @@ function ModalReorder(props) {
     return (
         <Modal
             visible={modal.visible}
-            title={<span><Icon type="appstore" /> 调整{scope_name(modal.scope)}顺序</span>}
+            title={<span><Icon type="appstore" /> 调整{scope_name(modal.scope)}顺序 <small style={{opacity: .6}}> 按住并拖动</small></span>}
             onCancel={()=>dispatch(close_modal())}
             onOk={do_post}
             destroyOnClose={true}
         >
-            {!!mod_list &&
-                <Reorder
-                    itemKey="id"
-                    lock="horizontal"
-                    holdTime={0}
-                    list={mod_list}
-                    template={ReorderListItem}
-                    callback={reorder_callback}
-                />
-            }
+            <div className="reorder-list-container">
+                {!!mod_list &&
+                    <Reorder
+                        itemKey="id"
+                        lock="horizontal"
+                        holdTime={100}
+                        list={mod_list}
+                        template={ReorderListItem}
+                        callback={reorder_callback}
+                    />
+                }
+            </div>
             {!!mod_list && mod_list.length===0 &&
                 <p>
                     <Icon type="inbox" /> 没有{scope_name(modal.scope)}
