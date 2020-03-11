095f802278d39b4521b4400e47c56c3f18d22d65
xmcp <xiaoyuanan2000@sina.com>
2020-03-11 23:13:24 +0800

    add select_single and select_multiple splash

--- src/welcome/SplashScreen.js
+++ src/welcome/SplashScreen.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {Result, Button, Checkbox} from 'antd';
+import {Result, Button, Checkbox, Radio} from 'antd';
 
 import {WithFooter} from '../app/Footer';
 
 import {do_splash_callback} from '../state/actions';
 
 import './SplashScreen.less';
+import {RobotOutlined} from '@ant-design/icons';
 
 function SplashAnnounceChecker(props) {
     const dispatch=useDispatch();
@@ -23,7 +24,9 @@ function SplashAnnounceChecker(props) {
                 <div style={{padding: '0 .5em'}}>
                     <div className="splash-body-container">
                         <h1>{props.handout.title}</h1>
-                        <div dangerouslySetInnerHTML={{__html: props.handout.instruction_html}} />
+                        {!!props.handout.instruction_html &&
+                            <div dangerouslySetInnerHTML={{__html: props.handout.instruction_html}} />
+                        }
                         <hr />
                         <div dangerouslySetInnerHTML={{__html: props.handout.content_html}} />
                         <hr />
@@ -51,6 +54,9 @@ function SplashAnnounce(props) {
                 <div style={{padding: '0 .5em'}}>
                     <div className="splash-body-container">
                         <h1>{props.handout.title}</h1>
+                        {!!props.handout.instruction_html &&
+                            <div dangerouslySetInnerHTML={{__html: props.handout.instruction_html}} />
+                        }
                         <hr />
                         <div dangerouslySetInnerHTML={{__html: props.handout.content_html}} />
                         <hr />
@@ -64,6 +70,75 @@ function SplashAnnounce(props) {
     );
 }
 
+function SplashSelectSingle(props) {
+    const dispatch=useDispatch();
+    const [selection,set_selection]=useState(props.handout.default_selection);
+
+    function do_post() {
+        dispatch(do_splash_callback(props.index,{selection: selection}));
+    }
+
+    return (
+        <WithFooter no_classname={true}>
+            <div className="width-container">
+                <div style={{padding: '0 .5em'}}>
+                    <div className="splash-body-container">
+                        <h1>{props.handout.title}</h1>
+                        {!!props.handout.instruction_html &&
+                        <div dangerouslySetInnerHTML={{__html: props.handout.instruction_html}} />
+                        }
+                        <hr />
+                        <div dangerouslySetInnerHTML={{__html: props.handout.content_html}} />
+                        <Radio.Group value={selection} onChange={(e)=>set_selection(e.target.value)}>
+                            {props.handout.selection.map(([value,text])=>(
+                                <Radio key={value} value={value} className="splash-boxify">{text}</Radio>
+                            ))}
+                        </Radio.Group>
+                    </div>
+                    <p>
+                        <Button type="primary" onClick={do_post}>继续</Button>
+                    </p>
+                </div>
+            </div>
+        </WithFooter>
+    );
+}
+
+function SplashSelectMultiple(props) {
+    const dispatch=useDispatch();
+    const [selection,set_selection]=useState(props.handout.default_selection||[]);
+
+    function do_post() {
+        dispatch(do_splash_callback(props.index,{selection: selection}));
+    }
+
+    return (
+        <WithFooter no_classname={true}>
+            <div className="width-container">
+                <div style={{padding: '0 .5em'}}>
+                    <div className="splash-body-container">
+                        <h1>{props.handout.title}</h1>
+                        {!!props.handout.instruction_html &&
+                            <div dangerouslySetInnerHTML={{__html: props.handout.instruction_html}} />
+                        }
+                        <hr />
+                        <div dangerouslySetInnerHTML={{__html: props.handout.content_html}} />
+                        <Checkbox.Group value={selection} onChange={set_selection}>
+                            {props.handout.selection.map(([value,text])=>(
+                                <Checkbox key={value} value={value} className="splash-boxify">{text}</Checkbox>
+                            ))}
+                        </Checkbox.Group>
+                    </div>
+                    <p>
+                        <Button type="primary" onClick={do_post}>继续</Button>
+                    </p>
+                </div>
+            </div>
+        </WithFooter>
+    );
+}
+
+
 export function SplashScreen(props) {
     const splash=useSelector((state)=>state.splash);
 
@@ -71,13 +146,18 @@ export function SplashScreen(props) {
         return (<SplashAnnounceChecker index={splash.index} handout={splash.handout} />);
     else if(splash.type==='announce')
         return (<SplashAnnounce index={splash.index} handout={splash.handout} />);
+    else if(splash.type==='select_single')
+        return (<SplashSelectSingle index={splash.index} handout={splash.handout} />);
+    else if(splash.type==='select_multiple')
+        return (<SplashSelectMultiple index={splash.index} handout={splash.handout} />);
     else
         return (
             <WithFooter>
                 <Result
+                    icon={<RobotOutlined />}
                     status="error"
                     title="无法显示页面"
-                    subTitle={'未知 Splash Screen：'+splash.type+' (#'+splash.index+')'}
+                    subTitle={'不支持 '+splash.name+' (#'+splash.index+', '+splash.type+')'}
                     extra={[
                         <Button onClick={()=>window.location.reload(true)}>刷新页面</Button>
                     ]}
